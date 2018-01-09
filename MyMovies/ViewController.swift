cf93c78e078b19f1ff9829947487abdaca66288b
Vinicius <vinicius.brito@ebix.com>
2018-01-09 17:39:59 -0200

    Initial Commit

---
+++ MyMovies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyMovies
+//
+//  Created by Vinicius Brito on 09/01/18.
+//  Copyright © 2018 Vinicius Brito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
